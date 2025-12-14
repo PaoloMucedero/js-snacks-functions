d95cda390bf33fa328e0ae61fa2f75b65b27d96a
PaoloMucedero <p.mucedero@gmail.com>
2025-12-14 23:12:41 +0100

    Consegna esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(string) { /* La funzione conta vocali di una data stringa */
+    let count = 0;/* Il conto deve ovviamente partire da zero */
+
+    for (let i = 0; i < string.length; i++) {/* Ciclo for per contare le vocali dentro la stringa, trattata come array */
+        if (
+            string[i] === "a" ||/* segno di differenza OR */
+            string[i] === "e" ||
+            string[i] === "i" ||
+            string[i] === "o" ||
+            string[i] === "u"
+        ) {
+            count++;/* Variabile per aumentare il contatore man mano che trova vocali */
+        }
+    }
+
+    return count;/* Salvataggio risultato del conteggio vocali a fine ciclo */
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
