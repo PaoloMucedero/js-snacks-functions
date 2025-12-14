9c7389b7ee106a4c327e8dfc40ecb7fced05791b
PaoloMucedero <p.mucedero@gmail.com>
2025-12-14 12:10:41 +0100

    Esercizio 1: aggiunta funzione arrow

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -8,8 +8,13 @@ function saluti(name){
     return "ciao " + name;
 }
 
+// FUNZIONE ARROW
+const saluta = name => "ciao " + name
+
+
 // Invoca la funzione qui e stampa il risultato in console
 console.log (saluti(userName))
-
+//saluta(userName);/* Perché funziona lo stesso se commento questa parte? */
+console.log(saluta(userName))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
